d8c81c4a56528ffd61856a9b4299df69eed9b8e8
kienphan <kientphan0@gmail.com>
2019-11-10 16:17:30 -0600

    Added routes and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Article;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+    return 204;
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
