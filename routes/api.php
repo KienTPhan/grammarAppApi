7556168e0f0c106e677548cc431570c3bdde4dfa
kienphan <kientphan0@gmail.com>
2019-11-10 17:36:47 -0600

    added authentication

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Article;
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-    return 204;
-});
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('logout', 'Auth\LoginController@logout');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('register', 'Auth\RegisterController@register');
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
 });
